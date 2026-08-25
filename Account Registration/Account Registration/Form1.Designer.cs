bb94c4da42300b96e197f90b3387e5cc74fdd4d0
Ryan Valencia <122194501+ritsukasu@users.noreply.github.com>
2026-08-25 20:11:24 +0800

    Add files via upload

---
+++ Account Registration/Account Registration/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Account_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblStudentNo = new Label();
+            lblProgram = new Label();
+            lblLastName = new Label();
+            lblFirstName = new Label();
+            lblMiddleName = new Label();
+            lblAge = new Label();
+            lblContactNo = new Label();
+            lblSpacer = new Label();
+            lblAddress = new Label();
+            txtStudentNo = new TextBox();
+            txtLastName = new TextBox();
+            txtFirstName = new TextBox();
+            txtContactNo = new TextBox();
+            txtAge = new TextBox();
+            txtMiddleName = new TextBox();
+            rtbAddress = new RichTextBox();
+            btnNext = new Button();
+            cbProgram = new ComboBox();
+            SuspendLayout();
+            // 
+            // lblStudentNo
+            // 
+            lblStudentNo.AutoSize = true;
+            lblStudentNo.Location = new Point(35, 28);
+            lblStudentNo.Name = "lblStudentNo";
+            lblStudentNo.Size = new Size(73, 15);
+            lblStudentNo.TabIndex = 0;
+            lblStudentNo.Text = "Student No.:";
+            lblStudentNo.Click += lblStudentNo_Click;
+            // 
+            // lblProgram
+            // 
+            lblProgram.AutoSize = true;
+            lblProgram.Location = new Point(208, 28);
+            lblProgram.Name = "lblProgram";
+            lblProgram.Size = new Size(53, 15);
+            lblProgram.TabIndex = 1;
+            lblProgram.Text = "Program";
+            lblProgram.Click += lblProgram_Click;
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Location = new Point(35, 93);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(63, 15);
+            lblLastName.TabIndex = 2;
+            lblLastName.Text = "Last Name";
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Location = new Point(208, 93);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(64, 15);
+            lblFirstName.TabIndex = 3;
+            lblFirstName.Text = "First Name";
+            // 
+            // lblMiddleName
+            // 
+            lblMiddleName.AutoSize = true;
+            lblMiddleName.Location = new Point(383, 93);
+            lblMiddleName.Name = "lblMiddleName";
+            lblMiddleName.RightToLeft = RightToLeft.No;
+            lblMiddleName.Size = new Size(79, 15);
+            lblMiddleName.TabIndex = 4;
+            lblMiddleName.Text = "Middle Name";
+            lblMiddleName.Click += lblMiddleName_Click;
+            // 
+            // lblAge
+            // 
+            lblAge.AutoSize = true;
+            lblAge.Location = new Point(35, 149);
+            lblAge.Name = "lblAge";
+            lblAge.Size = new Size(31, 15);
+            lblAge.TabIndex = 5;
+            lblAge.Text = "Age:";
+            lblAge.Click += lblAge_Click;
+            // 
+            // lblContactNo
+            // 
+            lblContactNo.AutoSize = true;
+            lblContactNo.Location = new Point(208, 149);
+            lblContactNo.Name = "lblContactNo";
+            lblContactNo.Size = new Size(74, 15);
+            lblContactNo.TabIndex = 6;
+            lblContactNo.Text = "Contact No.:";
+            // 
+            // lblSpacer
+            // 
+            lblSpacer.AutoSize = true;
+            lblSpacer.Location = new Point(427, 179);
+            lblSpacer.Name = "lblSpacer";
+            lblSpacer.Size = new Size(0, 15);
+            lblSpacer.TabIndex = 7;
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Location = new Point(35, 217);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(49, 15);
+            lblAddress.TabIndex = 8;
+            lblAddress.Text = "Address";
+            // 
+            // txtStudentNo
+            // 
+            txtStudentNo.Location = new Point(35, 54);
+            txtStudentNo.Name = "txtStudentNo";
+            txtStudentNo.Size = new Size(160, 23);
+            txtStudentNo.TabIndex = 9;
+            txtStudentNo.TextChanged += txtStudentNo_TextChanged;
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(35, 111);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(160, 23);
+            txtLastName.TabIndex = 11;
+            txtLastName.TextChanged += txtLastName_TextChanged;
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(208, 111);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(160, 23);
+            txtFirstName.TabIndex = 13;
+            txtFirstName.TextChanged += txtFirstName_TextChanged;
+            // 
+            // txtContactNo
+            // 
+            txtContactNo.Location = new Point(208, 171);
+            txtContactNo.Name = "txtContactNo";
+            txtContactNo.Size = new Size(160, 23);
+            txtContactNo.TabIndex = 14;
+            txtContactNo.TextChanged += txtContactNo_TextChanged;
+            // 
+            // txtAge
+            // 
+            txtAge.Location = new Point(35, 171);
+            txtAge.Name = "txtAge";
+            txtAge.Size = new Size(160, 23);
+            txtAge.TabIndex = 15;
+            txtAge.TextChanged += txtAge_TextChanged;
+            // 
+            // txtMiddleName
+            // 
+            txtMiddleName.Location = new Point(383, 111);
+            txtMiddleName.Name = "txtMiddleName";
+            txtMiddleName.Size = new Size(160, 23);
+            txtMiddleName.TabIndex = 16;
+            txtMiddleName.TextChanged += txtMiddleName_TextChanged;
+            // 
+            // rtbAddress
+            // 
+            rtbAddress.Location = new Point(35, 235);
+            rtbAddress.Name = "rtbAddress";
+            rtbAddress.Size = new Size(594, 117);
+            rtbAddress.TabIndex = 18;
+            rtbAddress.Text = "";
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(293, 358);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(75, 23);
+            btnNext.TabIndex = 19;
+            btnNext.Text = "Next";
+            btnNext.TextAlign = ContentAlignment.BottomCenter;
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click;
+            // 
+            // cbProgram
+            // 
+            cbProgram.FormattingEnabled = true;
+            cbProgram.Items.AddRange(new object[] { "Bachelor of Science in Information Technology (BSIT)", "Bachelor of Science in Computer Science (BSCS)", "BS in Civil Engineering (BSCE)", "BS in Mechanical Engineering (BSME)", "BS in Electrical Engineering (BSEE)", "BS in Accountancy (BSA)", "BS in Management Accounting (BSMA)", "BS in Business Administration (Financial/Marketing Management)", "BS in Hospitality Management (BSHM)", "BS in Tourism Management (BSTM)", "BS in Elementary Education (BEEd)", "BS in Secondary Education (BSEd)", "BS in Physical Education (BPEd)", "BS in Early Childhood Education (BECEd)", "BS in Special Needs Education (BSNEd)", "Bachelor of Arts in Communication (BA Comm)", "Bachelor of Arts in English Language (BAEL)", "BS in Psychology (BS Psych)", "BS in Mathematics (BS Math)", "BS in Criminology (BS Crim)" });
+            cbProgram.Location = new Point(208, 54);
+            cbProgram.Name = "cbProgram";
+            cbProgram.Size = new Size(421, 23);
+            cbProgram.TabIndex = 20;
+            cbProgram.SelectedIndexChanged += cbProgram_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(653, 413);
+            Controls.Add(cbProgram);
+            Controls.Add(btnNext);
+            Controls.Add(rtbAddress);
+            Controls.Add(txtMiddleName);
+            Controls.Add(txtAge);
+            Controls.Add(txtContactNo);
+            Controls.Add(txtFirstName);
+            Controls.Add(txtLastName);
+            Controls.Add(txtStudentNo);
+            Controls.Add(lblAddress);
+            Controls.Add(lblSpacer);
+            Controls.Add(lblContactNo);
+            Controls.Add(lblAge);
+            Controls.Add(lblMiddleName);
+            Controls.Add(lblFirstName);
+            Controls.Add(lblLastName);
+            Controls.Add(lblProgram);
+            Controls.Add(lblStudentNo);
+            Name = "Form1";
+            Text = "FrmRegistration";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStudentNo;
+        private Label lblProgram;
+        private Label lblLastName;
+        private Label lblFirstName;
+        private Label lblMiddleName;
+        private Label lblAge;
+        private Label lblContactNo;
+        private Label lblSpacer;
+        private Label lblAddress;
+        private TextBox txtStudentNo;
+        private TextBox txtLastName;
+        private TextBox txtFirstName;
+        private TextBox txtContactNo;
+        private TextBox txtAge;
+        private TextBox txtMiddleName;
+        private RichTextBox rtbAddress;
+        private Button btnNext;
+        private ComboBox cbProgram;
+    }
+}
---
+++ Account Registration/Account Registration/FrmConfirm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Account_Registration
+{
+    partial class FrmConfirm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            lblStudentNoCaption = new Label();
+            lblStudentNo = new Label();
+            lblProgramCaption = new Label();
+            lblProgram = new Label();
+            lblLastNameCaption = new Label();
+            lblLastName = new Label();
+            lblFirstNameCaption = new Label();
+            lblFirstName = new Label();
+            lblMiddleNameCaption = new Label();
+            lblMiddleName = new Label();
+            lblAgeCaption = new Label();
+            lblAge = new Label();
+            lblContactNoCaption = new Label();
+            lblContactNo = new Label();
+            lblAddressCaption = new Label();
+            lblAddress = new Label();
+            btnSubmit = new Button();
+            SuspendLayout();
+            // 
+            // lblStudentNoCaption
+            // 
+            lblStudentNoCaption.AutoSize = true;
+            lblStudentNoCaption.Location = new Point(34, 33);
+            lblStudentNoCaption.Name = "lblStudentNoCaption";
+            lblStudentNoCaption.Size = new Size(90, 20);
+            lblStudentNoCaption.TabIndex = 0;
+            lblStudentNoCaption.Text = "Student No.:";
+            // 
+            // lblStudentNo
+            // 
+            lblStudentNo.AutoSize = true;
+            lblStudentNo.Location = new Point(171, 33);
+            lblStudentNo.Name = "lblStudentNo";
+            lblStudentNo.Size = new Size(13, 20);
+            lblStudentNo.TabIndex = 1;
+            lblStudentNo.Text = " ";
+            // 
+            // lblProgramCaption
+            // 
+            lblProgramCaption.AutoSize = true;
+            lblProgramCaption.Location = new Point(34, 73);
+            lblProgramCaption.Name = "lblProgramCaption";
+            lblProgramCaption.Size = new Size(69, 20);
+            lblProgramCaption.TabIndex = 2;
+            lblProgramCaption.Text = "Program:";
+            // 
+            // lblProgram
+            // 
+            lblProgram.AutoSize = true;
+            lblProgram.Location = new Point(171, 73);
+            lblProgram.Name = "lblProgram";
+            lblProgram.Size = new Size(13, 20);
+            lblProgram.TabIndex = 3;
+            lblProgram.Text = " ";
+            // 
+            // lblLastNameCaption
+            // 
+            lblLastNameCaption.AutoSize = true;
+            lblLastNameCaption.Location = new Point(34, 113);
+            lblLastNameCaption.Name = "lblLastNameCaption";
+            lblLastNameCaption.Size = new Size(79, 20);
+            lblLastNameCaption.TabIndex = 4;
+            lblLastNameCaption.Text = "Last name:";
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Location = new Point(171, 113);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(13, 20);
+            lblLastName.TabIndex = 5;
+            lblLastName.Text = " ";
+            // 
+            // lblFirstNameCaption
+            // 
+            lblFirstNameCaption.AutoSize = true;
+            lblFirstNameCaption.Location = new Point(34, 153);
+            lblFirstNameCaption.Name = "lblFirstNameCaption";
+            lblFirstNameCaption.Size = new Size(80, 20);
+            lblFirstNameCaption.TabIndex = 6;
+            lblFirstNameCaption.Text = "First name:";
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Location = new Point(171, 153);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(13, 20);
+            lblFirstName.TabIndex = 7;
+            lblFirstName.Text = " ";
+            // 
+            // lblMiddleNameCaption
+            // 
+            lblMiddleNameCaption.AutoSize = true;
+            lblMiddleNameCaption.Location = new Point(34, 193);
+            lblMiddleNameCaption.Name = "lblMiddleNameCaption";
+            lblMiddleNameCaption.Size = new Size(103, 20);
+            lblMiddleNameCaption.TabIndex = 8;
+            lblMiddleNameCaption.Text = "Middle Name:";
+            // 
+            // lblMiddleName
+            // 
+            lblMiddleName.AutoSize = true;
+            lblMiddleName.Location = new Point(171, 193);
+            lblMiddleName.Name = "lblMiddleName";
+            lblMiddleName.Size = new Size(13, 20);
+            lblMiddleName.TabIndex = 9;
+            lblMiddleName.Text = " ";
+            // 
+            // lblAgeCaption
+            // 
+            lblAgeCaption.AutoSize = true;
+            lblAgeCaption.Location = new Point(34, 233);
+            lblAgeCaption.Name = "lblAgeCaption";
+            lblAgeCaption.Size = new Size(39, 20);
+            lblAgeCaption.TabIndex = 10;
+            lblAgeCaption.Text = "Age:";
+            // 
+            // lblAge
+            // 
+            lblAge.AutoSize = true;
+            lblAge.Location = new Point(171, 233);
+            lblAge.Name = "lblAge";
+            lblAge.Size = new Size(13, 20);
+            lblAge.TabIndex = 11;
+            lblAge.Text = " ";
+            // 
+            // lblContactNoCaption
+            // 
+            lblContactNoCaption.AutoSize = true;
+            lblContactNoCaption.Location = new Point(34, 273);
+            lblContactNoCaption.Name = "lblContactNoCaption";
+            lblContactNoCaption.Size = new Size(90, 20);
+            lblContactNoCaption.TabIndex = 12;
+            lblContactNoCaption.Text = "Contact No.:";
+            // 
+            // lblContactNo
+            // 
+            lblContactNo.AutoSize = true;
+            lblContactNo.Location = new Point(171, 273);
+            lblContactNo.Name = "lblContactNo";
+            lblContactNo.Size = new Size(13, 20);
+            lblContactNo.TabIndex = 13;
+            lblContactNo.Text = " ";
+            // 
+            // lblAddressCaption
+            // 
+            lblAddressCaption.AutoSize = true;
+            lblAddressCaption.Location = new Point(34, 313);
+            lblAddressCaption.Name = "lblAddressCaption";
+            lblAddressCaption.Size = new Size(65, 20);
+            lblAddressCaption.TabIndex = 14;
+            lblAddressCaption.Text = "Address:";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Location = new Point(171, 313);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(13, 20);
+            lblAddress.TabIndex = 15;
+            lblAddress.Text = " ";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(228, 381);
+            btnSubmit.Margin = new Padding(3, 4, 3, 4);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(103, 40);
+            btnSubmit.TabIndex = 0;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // FrmConfirm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(595, 467);
+            Controls.Add(lblStudentNoCaption);
+            Controls.Add(lblStudentNo);
+            Controls.Add(lblProgramCaption);
+            Controls.Add(lblProgram);
+            Controls.Add(lblLastNameCaption);
+            Controls.Add(lblLastName);
+            Controls.Add(lblFirstNameCaption);
+            Controls.Add(lblFirstName);
+            Controls.Add(lblMiddleNameCaption);
+            Controls.Add(lblMiddleName);
+            Controls.Add(lblAgeCaption);
+            Controls.Add(lblAge);
+            Controls.Add(lblContactNoCaption);
+            Controls.Add(lblContactNo);
+            Controls.Add(lblAddressCaption);
+            Controls.Add(lblAddress);
+            Controls.Add(btnSubmit);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "FrmConfirm";
+            Text = "FrmConfirm";
+            FormClosing += FrmConfirm_FormClosing;
+            Load += FrmConfirm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblStudentNoCaption;
+        private System.Windows.Forms.Label lblStudentNo;
+        private System.Windows.Forms.Label lblProgramCaption;
+        private System.Windows.Forms.Label lblProgram;
+        private System.Windows.Forms.Label lblLastNameCaption;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstNameCaption;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblMiddleNameCaption;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.Label lblAgeCaption;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblContactNoCaption;
+        private System.Windows.Forms.Label lblContactNo;
+        private System.Windows.Forms.Label lblAddressCaption;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
